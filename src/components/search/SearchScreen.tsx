c80b9d653be719a35a5464a0cb536fe34b932181
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2020-06-30 17:01:37 -0400

    created onsearch query

--- src/components/search/SearchScreen.tsx
+++ src/components/search/SearchScreen.tsx
@@ -1,53 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLazyQuery } from "@apollo/react-hooks";
 import { StyleSheet, TextInput, FlatList } from "react-native";
 import { Box, Text } from "react-native-design-utility";
 
 import KeyboardDismissView from "../../components/KeyboardDismissView";
 
 import { theme } from "../../constants/theme";
+import { SearchQueryVariables } from "../../types/graphql";
+import searchQuery from "../../graphql/query/searchQuery";
 
 const SearchScreen = () => {
-  return (
-    <KeyboardDismissView>
-      <Box f={1} bg="white">
-        <Box h={50} w="100%" px="sm" my="sm">
-          <TextInput
-            placeholder="Search Podcast"
-            selectionColor={theme.color.blueLight}
-            style={{
-              height: 40,
-              flex: 1,
-              backgroundColor: theme.color.greyLightest,
-              borderRadius: 10,
-              paddingHorizontal: theme.space.sm,
-              fontSize: theme.text.size.md
-            }}
-          />
-        </Box>
+  const [term, setTerm] = useState<string>("");
 
-        <FlatList
+  const [, { data, loading, error }] = useLazyQuery<
+    SearchQuery,
+    SearchQueryVariables
+  >(searchQuery);
+
+  const onSearch = async () => {
+    try {
+      await search({
+        variables: {
+          term
+        }
+      });
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  return (
+    <Box f={1} bg="white">
+      <Box h={50} w="100%" px="sm" my="sm">
+        <TextInput
+          placeholder="Search Podcast"
+          selectionColor={theme.color.blueLight}
           style={{
-            minHeight: "100%"
+            height: 40,
+            flex: 1,
+            backgroundColor: theme.color.greyLightest,
+            borderRadius: 10,
+            paddingHorizontal: theme.space.sm,
+            fontSize: theme.text.size.md
           }}
-          data={[{ id: 1 }, { id: 2 }]}
-          renderItem={() => (
-            <Box h={90} dir="row" align="center">
-              <Box h={70} w={70} bg="blueLight" radius={10} mr={10} />
-              <Box>
-                <Text bold>Podcast</Text>
-                <Text size="xs" color="grey">
-                  Subtitle
-                </Text>
-                <Text size="xs" color="blueLight">
-                  275 Episodes
-                </Text>
-              </Box>
-            </Box>
-          )}
-          keyExtractor={(item) => String(item.id)}
         />
       </Box>
-    </KeyboardDismissView>
+
+      <FlatList
+        keyboardShouldPersistTaps="never"
+        style={{
+          minHeight: "100%"
+        }}
+        data={[{ id: 1 }, { id: 2 }]}
+        renderItem={() => (
+          <Box h={90} dir="row" align="center">
+            <Box h={70} w={70} bg="blueLight" radius={10} mr={10} />
+            <Box>
+              <Text bold>Podcast</Text>
+              <Text size="xs" color="grey">
+                Subtitle
+              </Text>
+              <Text size="xs" color="blueLight">
+                275 Episodes
+              </Text>
+            </Box>
+          </Box>
+        )}
+        keyExtractor={(item) => String(item.id)}
+      />
+    </Box>
   );
 };
 
